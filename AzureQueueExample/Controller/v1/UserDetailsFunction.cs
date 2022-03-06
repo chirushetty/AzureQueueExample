b1b8ee73857bcbf8533818b085bb7b4e94f1625b
ChiragShetty1986 <chirag.shetty@newday.co.uk>
2022-03-06 01:25:48 +0000

    Added controller and infrastructure project and storageClient

---
+++ AzureQueueExample/Controller/v1/UserDetailsFunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureQueueExample.Controller.v1
+{
+    public class UserDetailsFunction
+    {
+        private readonly ILogger<UserDetailsFunction> _logger;
+
+        public UserDetailsFunction(ILogger<UserDetailsFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [FunctionName(nameof(UserDetailsFunction))]
+        public async Task<IActionResult> HandleEvent(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/userDetails")]
+            HttpRequest req,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                _logger.LogInformation($"{nameof(UserDetailsFunction)} invoked");
+
+                await Task.FromResult("");
+                return new OkObjectResult("");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return new InternalServerErrorResult();
+            }
+        }
+    }
+}
